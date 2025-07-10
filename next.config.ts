083a04b5cd609a4df0244db17109ebc23c2168a9
Karol Wojtyla <karolwojtyla360@gmail.com>
2025-07-10 20:59:09 -0300

    refactor(next-config): add remote pattern for image importation

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,12 @@ import createNextIntlPlugin from "next-intl/plugin";
 
 const nextConfig: NextConfig = {
   images: {
-    domains: ["cdn.sanity.io"],
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "cdn.sanity.io",
+      },
+    ],
   },
 };
 
